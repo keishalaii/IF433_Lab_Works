6046427a7de02055b0c3e5fd92ee7bca2bef2da5
Keisha Aria Lai <keishalai6@gmail.com>
2026-02-05 01:24:48 +0700

    week01: add calculateStatus function

--- src/oop_00000128606_keishaarialai/week01/GradingSystem.kt
+++ src/oop_00000128606_keishaarialai/week01/GradingSystem.kt
@@ -13,4 +13,7 @@ fun main(){
         else -> "D"
     }
     println("Grade: $grade")
+    println("Status: ${calculateStatus(score)}")
 }
+
+fun calculateStatus(score: Int) = if (score > 75) "Lulus" else "Tidak Lulus"
