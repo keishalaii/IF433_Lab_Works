ac15a02e78427fd4cb1965b01bcccd15423d504a
Keisha Aria Lai <keishalai6@gmail.com>
2026-02-09 21:44:10 +0700

    Week01: (tugas 1) clean up unused argos and comments

--- src/oop_00000128606_keishaarialai/week01/CircleCalculator.kt
+++ src/oop_00000128606_keishaarialai/week01/CircleCalculator.kt
@@ -1,21 +1,15 @@
 package oop_00000128606_keishaarialai.week01
 
-fun main(args: Array<String>) {
-    // Variable definition
+fun main() {
     val radius = 7.0
     val pi = 3.14
 
-    // Calculation
     val area = pi * radius * radius
 
-    // Output Concatenation
     println("Radius: $radius, Area: $area")
-
-    // Logic check
-    checkSize(area)
+    println(checkSize(area))
 }
 
 fun checkSize(area: Double) =
     if (area > 100) "This is a Big Circle"
     else "This is a Small Circle"
-}
