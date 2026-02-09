cca870ca3062fb40fe48f50b5d22bdf7782c3c3a
Keisha Aria Lai <keishalai6@gmail.com>
2026-02-09 22:33:36 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000128606_keishaarialai/week01/GameStore.kt
+++ src/oop_00000128606_keishaarialai/week01/GameStore.kt
@@ -4,9 +4,12 @@ fun main() {
     val gameTitle = "Grand Theft Auto V"
     val price = 500000
 
-    println(calculateDiscount(price))
+    printReceipt(title = gameTitle, calculateDiscount(price))
 }
 
 fun calculateDiscount(price: Int): Int =
     if (price > 500000) return price - price * 20/100
     else return price - price * 10/100
+
+fun printReceipt(title: String, finalPrice: Int) =
+    println("Game TItle: $title \nFinal Price: $finalPrice")
