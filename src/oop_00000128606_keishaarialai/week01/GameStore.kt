a20ebd408d629cba51f91fc4f5830158bf79e580
Keisha Aria Lai <keishalai6@gmail.com>
2026-02-09 22:19:40 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000128606_keishaarialai/week01/GameStore.kt
+++ src/oop_00000128606_keishaarialai/week01/GameStore.kt
@@ -3,4 +3,10 @@ package oop_00000128606_keishaarialai.week01
 fun main() {
     val gameTitle = "Grand Theft Auto V"
     val price = 500000
+
+    println(calculateDiscount(price))
 }
+
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000) return price - price * 20/100
+    else return price - price * 10/100
