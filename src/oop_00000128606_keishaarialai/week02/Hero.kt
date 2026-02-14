520d690fbba8e675043ebce9c07163c4db82bd92
kei <keishalai6@gmail.com>
2026-02-15 01:37:21 +0700

    week02: (task 2) implement the game

--- src/oop_00000128606_keishaarialai/week02/Hero.kt
+++ src/oop_00000128606_keishaarialai/week02/Hero.kt
@@ -1,5 +1,7 @@
 package oop_00000128606_keishaarialai.week02
 
+import java.util.Scanner
+
 class Hero(
     val name: String,
     val baseDamage: Int,
@@ -16,5 +18,55 @@ class Hero(
     }
     fun isAlive(): Boolean {
         return hp > 0
+    }
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    println("=== MINI RPG BATTLE ===")
+
+    print("Masukkan Nama Hero: ")
+    val heroName = scanner.nextLine()
+
+    print("Masukkan Base Damage Hero: ")
+    val damage = scanner.nextInt()
+
+    val hero = Hero(heroName, damage)
+    var enemyHp = 100
+    println("\nMusuh muncul dengan HP 100!")
+
+    while (hero.isAlive() && enemyHp > 0) {
+        println("\nMenu:")
+        println("1. Serang")
+        println("2. Kabur")
+        print("Pilih: ")
+
+        val choice = scanner.nextInt()
+        if (choice == 1) {
+            hero.attack("Musuh")
+            enemyHp -= hero.baseDamage
+            if (enemyHp < 0) enemyHp = 0
+            println("Sisa HP Musuh: $enemyHp")
+            if (enemyHp > 0) {
+                val enemyDamage = (10..20).random()
+                println("Musuh menyerang balik sebesar $enemyDamage!")
+                hero.takeDamage(enemyDamage)
+                println("Sisa HP ${hero.name}: ${hero.hp}")
+            }
+        } else if (choice == 2) {
+            println("${hero.name} kabur dari pertarungan!")
+            break
+        } else {
+            println("Pilihan tidak valid!")
+        }
+    }
+    println("\n=== HASIL PERTARUNGAN ===")
+
+    if (hero.hp > enemyHp) {
+        println("${hero.name} Menang!")
+    } else if (enemyHp > hero.hp) {
+        println("Musuh Menang!")
+    } else {
+        println("Seri!")
     }
 }
