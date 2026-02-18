636e06171c9ec05b9e09345225e2509b382ce23b
kei <keishalai6@gmail.com>
2026-02-18 13:59:18 +0700

    Week3 Theory Praktek pakai metode private

--- src/oop_00000128606_keishaarialai/week03/PapiKeii.kt
+++ src/oop_00000128606_keishaarialai/week03/PapiKeii.kt
@@ -3,13 +3,42 @@ package oop_00000128606_keishaarialai.week03
 //W3_TheoryPraktek
 
 class PapiKeii {
-    public var nama: String = "Belum tau"
-    public var umur: Int = 35
+    private var nama: String = "Belum tau"
+    private var umur: Int = 35
+    private var umurValid: Boolean = true
+
+    fun set_nama(namaBaru: String) {
+        if (namaBaru.isBlank()) {
+            println("Eh nama gaboleh kosong")
+        } else {
+            this.nama = namaBaru
+        }
+    }
+
+    fun set_umur(umurBaru: Int) {
+        if (umurBaru < -5) {
+            println("umur tidak boleh dibawah -5 yaa")
+            umurValid = false
+        } else {
+            this.umur = umurBaru
+            umurValid = true
+        }
+    }
+
+    fun get_nama(): String {
+        return nama
+    }
+
+    fun get_umur(): String {
+        return if (umurValid) umur.toString()
+        else "tidak diketahui"
+    }
 }
+
 
 fun main() {
     var papi = PapiKeii()
-    papi.nama = "Lai"
-    papi.umur = 55
-    println("Nama ayah kamu: ${papi.nama} umur-nya ${papi.umur}") //bisa pake (+ papi.nama) juga
+    papi.set_nama("Lai")
+    papi.set_umur(-7)
+    println("Nama ayah kamu ${papi.get_nama()} Umur-nya ${papi.get_umur()}") //bisa pake (+ papi.nama) juga
 }
