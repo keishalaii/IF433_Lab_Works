9306ebfbdf2666033f391f2906845a6da0ce9e04
kei <keishalai6@gmail.com>
2026-02-25 15:38:07 +0700

    week04: (task 1) test ElectricCar in main

--- src/oop_00000128606_keishaarialai/week04/Main.kt
+++ src/oop_00000128606_keishaarialai/week04/Main.kt
@@ -11,4 +11,10 @@ fun main(){
     myCar.openTrunk() //panggil method milik sendiri
     myCar.honk() //panggil method yg sudah di override
     myCar.accelerate() //panggil gabungan method parent dan child
+
+    println("\n--- Testing Electric Car ---")
+    val myElectricCar = ElectricCar("Tesla", 4, 85)
+    myElectricCar.accelerate()
+    myElectricCar.honk()
+    myElectricCar.openTrunk()
 }
