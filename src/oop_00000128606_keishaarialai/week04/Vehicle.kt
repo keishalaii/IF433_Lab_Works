759003b808fc46fe8503c6afabf26b6edd44ac52
kei <keishalai6@gmail.com>
2026-02-25 14:57:45 +0700

    week04: create basic Vehicle parent class

---
+++ src/oop_00000128606_keishaarialai/week04/Vehicle.kt
@@ -0,0 +1,16 @@
+package oop_00000128606_keishaarialai.week04
+
+//menggunakan keyword 'open' agar class bisa diwariskan
+open class Vehicle(val brand: String) {
+    var speed: Int = 0
+
+    //pakai 'open' supaya bisa di override
+    open fun accelerate() {
+        speed += 10
+        println("$brand melaju dengan kecepatan $speed km/jam")
+    }
+
+    open fun honk(){
+        println("Beep beep!")
+    }
+}
