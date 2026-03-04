69bb79592acb9e54032d84b1b9e445de4e00fefa
kei <keishalai6@gmail.com>
2026-03-04 15:41:21 +0700

    week05: (task 1) test MathHelper overloading in main

--- src/oop_00000128606_keishaarialai/week05/MathHelper.kt
+++ src/oop_00000128606_keishaarialai/week05/MathHelper.kt
@@ -1,4 +1,5 @@
 package oop_00000128606_keishaarialai.week05
+import java.util.Scanner // saya pakai scanner
 
 class MathHelper {
 
@@ -16,4 +17,47 @@ class MathHelper {
     fun hitungLuas(jariJari: Double): Double {
         return 3.14 * jariJari * jariJari
     }
+}
+
+fun main() {
+    val scanner = Scanner(System.`in`)
+    val mathHelper = MathHelper()
+
+    println("==========================================")
+    println("   PROGRAM MENGHITUNG LUAS BANGUN DATAR")
+    println("==========================================")
+    println()
+
+    // 1. Menghitung Luas Persegi
+    println("--- MENGHITUNG LUAS PERSEGI ---")
+    print("Masukkan panjang sisi persegi: ")
+    val sisi = scanner.nextInt()
+    val luasPersegi = mathHelper.hitungLuas(sisi)
+    println("Rumus: sisi x sisi")
+    println("sisi = $sisi")
+    println("Luas Persegi = $luasPersegi")
+    println()
+
+    // 2. Menghitung Luas Persegi Panjang
+    println("--- MENGHITUNG LUAS PERSEGI PANJANG ---")
+    print("Masukkan panjang: ")
+    val panjang = scanner.nextInt()
+    print("Masukkan lebar: ")
+    val lebar = scanner.nextInt()
+    val luasPersegiPanjang = mathHelper.hitungLuas(panjang, lebar)
+    println("Rumus: panjang x lebar")
+    println("panjang = $panjang, lebar = $lebar")
+    println("Luas Persegi Panjang = $luasPersegiPanjang")
+    println()
+
+    // 3. Menghitung Luas Lingkaran
+    println("--- MENGHITUNG LUAS LINGKARAN ---")
+    print("Masukkan jari-jari lingkaran: ")
+    val jariJari = scanner.nextDouble()
+    val luasLingkaran = mathHelper.hitungLuas(jariJari)
+    println("Rumus: 3.14 x jari-jari x jari-jari")
+    println("jari-jari = $jariJari")
+    println("Luas Lingkaran = $luasLingkaran")
+
+    scanner.close()
 }
