ed94141e1e9f23ab44b3b405280227f532bedc11
kei <keishalai6@gmail.com>
2026-03-04 16:10:15 +0700

    week05: (task 2) add smart casting for EWallet top up recovery

--- src/oop_00000128606_keishaarialai/week05/Task2Main.kt
+++ src/oop_00000128606_keishaarialai/week05/Task2Main.kt
@@ -13,4 +13,24 @@ fun main() {
         println(method.accountName + ":")
         method.processPayment(75000.0)
     }
+    println("-------------------------------------")
+
+    // Smart Casting: Deteksi EWallet dan Top Up
+    println("Smart Casting - Deteksi EWallet:")
+    for(method in paymentMethods) {
+        println("Memeriksa " + method.accountName + ": ")
+        if(method is EWallet) {
+            println("-> Terdeteksi EWallet. Melakukan top up Rp 50.000")
+            method.topUp(50000.0)
+            println("-> Mencoba pembayaran Rp 75.000 lagi:")
+            method.processPayment(75000.0)
+        } else {
+            println("Bukan EWallet (CreditCard)")
+        }
+        println()
+    }
+
+    println("Status Akhir:")
+    println("GoPay saldo: Rp " + eWallet.balance)
+    println("Visa terpakai: Rp " + creditCard.usedAmount + ", sisa limit: Rp " + (creditCard.limit - creditCard.usedAmount))
 }
