f4ebb8e3db2606c735da71bee6ba3beef1ccc83a
kei <keishalai6@gmail.com>
2026-03-11 15:32:49 +0700

    week06: create Smartwatch with multiple implementations

---
+++ src/oop_00000128606_keishaarialai/week06/Smartwatch.kt
@@ -0,0 +1,15 @@
+package oop_00000128606_keishaarialai.week06
+
+class Smartwatch : watch(), BluetoothConnectable, Rechargeable {
+    override fun showTime() {
+        println("Layar OLED menyala: 14:00 WIB")
+    }
+
+    override fun connectToBluetooth() {
+        println("Mencari perangkat HP di sekitar untuk pairing...")
+    }
+
+    override fun chargeBattery() {
+        println("Mengisi daya menggunakan charger megnetik 15W.")
+    }
+}
