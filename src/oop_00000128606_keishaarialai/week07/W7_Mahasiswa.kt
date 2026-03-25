59e692acd8ab044b98955d73270d0fefeab54310
kei <keishalai6@gmail.com>
2026-03-25 13:33:48 +0700

    Belajar pakai companion object (cara 2)

---
+++ src/oop_00000128606_keishaarialai/week07/W7_Mahasiswa.kt
@@ -0,0 +1,11 @@
+package oop_00000128606_keishaarialai.week07
+
+class Mahasiswa {
+    companion object {
+        var nama:String = "Siapa"
+
+        fun set_nilai(uts:Int, uas:Int):Int{
+            return (uts+uas)/2
+        }
+    }
+}
--- src/oop_00000128606_keishaarialai/week07/W7_MainProgram.kt
+++ src/oop_00000128606_keishaarialai/week07/W7_MainProgram.kt
@@ -3,4 +3,8 @@ package oop_00000128606_keishaarialai.week07
 fun main() {
     println("Server: ${Koneksi.LinkServer}")
     Koneksi.test_koneksi()
+
+    Mahasiswa.nama = "Kei"
+    val na:Int = Mahasiswa.set_nilai(80,100)
+    println("Nilai ${Mahasiswa.nama} adalah $na")
 }
