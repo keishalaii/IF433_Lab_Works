e3d24f3bdee6afcf639305db7deb0217d5e44e34
kei <keishalai6@gmail.com>
2026-03-25 15:58:25 +0700

    Week07: (task) implement factory pattern in WeaponForge

---
+++ src/oop_00000128606_keishaarialai/week07/WeaponForge.kt
@@ -0,0 +1,15 @@
+package oop_00000128606_keishaarialai.week07
+
+class Weapon private constructor(val item: GameItem, val durability: Int) {
+    companion object {
+        fun forgeStarterSword(): Weapon {
+            val item = GameItem("Pedang Kayu Bapuk", 5, ItemRarity.COMMON)
+            return Weapon(item, 50)
+        }
+
+        fun forgeEpicSword(): Weapon {
+            val item = GameItem("Pedang Naga Legendaris", 100, ItemRarity.EPIC)
+            return Weapon(item, 200)
+        }
+    }
+}
