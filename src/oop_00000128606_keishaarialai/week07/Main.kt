e83ebf41f9160e0797726a64a9eacccaf2d4c947
kei <keishalai6@gmail.com>
2026-03-25 15:25:45 +0700

    Week07: test Singleton and Companion Object in main

--- src/oop_00000128606_keishaarialai/week07/Main.kt
+++ src/oop_00000128606_keishaarialai/week07/Main.kt
@@ -1,7 +1,13 @@
 package oop_00000128606_keishaarialai.week07
 
+import java.net.NetworkInterface
+
 fun main() {
-    // Mencoba membuat instance langsung (HARUS ERROR)
-    val client = NetworkClient("https://api.umn.ac.id")
+    println("=== TEST SINGLETON ===")
+    println("Status: ${DatabaseManager.connectionStatus}")
+    DatabaseManager.connect()
+
+    println("\n=== TEST COMPANION OBJECT ===")
+    val client = NetworkClient.createClient() //instansiasi lewat factory
     client.connect()
 }
