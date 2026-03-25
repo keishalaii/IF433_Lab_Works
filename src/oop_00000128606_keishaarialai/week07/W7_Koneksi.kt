75bf89e8fa65c3c9f20a51e648a862e27c3b6ee2
kei <keishalai6@gmail.com>
2026-03-25 13:22:46 +0700

    Belajar pakai object (cara 1)

---
+++ src/oop_00000128606_keishaarialai/week07/W7_Koneksi.kt
@@ -0,0 +1,9 @@
+package oop_00000128606_keishaarialai.week07
+
+object Koneksi {
+    val LinkServer: String = "http://localhost:8080"
+
+    fun test_koneksi() {
+        println("Koneksi sukses dari $LinkServer")
+    }
+}
---
+++ src/oop_00000128606_keishaarialai/week07/W7_MainProgram.kt
@@ -0,0 +1,6 @@
+package oop_00000128606_keishaarialai.week07
+
+fun main() {
+    println("Server: ${Koneksi.LinkServer}")
+    Koneksi.test_koneksi()
+}
