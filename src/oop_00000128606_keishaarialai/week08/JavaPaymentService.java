6973c7767baa3cc053610af0fb8198944ba8d090
kei <keishalai6@gmail.com>
2026-04-15 16:33:21 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000128606_keishaarialai/week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000128606_keishaarialai.week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
