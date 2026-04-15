ba0f0765218d5682ca95b399a47195b873f21ae5
kei <keishalai6@gmail.com>
2026-04-15 14:49:51 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_00000128606_keishaarialai/week08/UserProfile.kt
@@ -0,0 +1,8 @@
+package oop_00000128606_keishaarialai.week08
+
+//nama: Non-null (WAJIB), email & phone: Nullable (Optional)
+class UserProfile(
+    val name: String,
+    val email: String?,
+    val phone: String? = null
+)
