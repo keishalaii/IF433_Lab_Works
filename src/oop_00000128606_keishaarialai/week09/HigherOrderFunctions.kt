481b5ed2b47e3babff0097fc6cc6fefabb363296
kei <keishalai6@gmail.com>
2026-04-22 15:32:52 +0700

    week09: implement sortedByDescending and forEach

--- src/oop_00000128606_keishaarialai/week09/HigherOrderFunctions.kt
+++ src/oop_00000128606_keishaarialai/week09/HigherOrderFunctions.kt
@@ -1,9 +1,17 @@
 package oop_00000128606_keishaarialai.week09
 
+import jdk.internal.net.http.frame.Http2Frame.asString
+
 fun main() {
     val numbers = listOf(1, 2, 3, 4, 5, 6, 7, 8)
     println("Original Data: $numbers")
     println("\n=== HOF: FILTER ===")
     val evens = numbers.filter { it % 2 == 0 }
     println("Evens only: $evens")
+
+    println("\n=== HOF: MAP ===")
+    val multiplied = evens.map { it * 10 }
+    val asString = multiplied.map { "Rp ${it}k" }
+    println("Multiplied $multiplied")
+    println("Formatted: $asString")
 }
