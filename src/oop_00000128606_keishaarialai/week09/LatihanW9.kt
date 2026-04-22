4390d29fc710fc13e570118968a7dc9a16a3d036
kei <keishalai6@gmail.com>
2026-04-22 13:43:17 +0700

    Belajar Set dan MutableSet (Array) Week 9

--- src/oop_00000128606_keishaarialai/week09/LatihanW9.kt
+++ src/oop_00000128606_keishaarialai/week09/LatihanW9.kt
@@ -1,7 +1,7 @@
 package oop_00000128606_keishaarialai.week09
 
 fun main() {
-    println("========= LIST OF =========")
+    println("\n========= LIST OF =========")
     val ArSiswa = listOf("Budi", "Danu", "Susi", "Cica")
     //view
     println(ArSiswa)
@@ -23,4 +23,17 @@ fun main() {
     ArMatkul[2] = "Aljabar Linear" //ngedit
     ArMatkul.remove("Web") //hapus
     println(ArMatkul)
+
+    println("\n========= SET OF IMUT =========")
+    val ArNilai = setOf(80, 90, 75, 85, 80, 30, 50, 85)
+    println("Banyak Nilai: ${ArNilai.size}")
+    println("Ada angka 90 gak? ${ArNilai.contains(90)}")
+    println(ArNilai)
+
+    println("\n========= SET OF MUTABLE =========")
+    val ArMahasiswa = mutableSetOf("Andi", "Yanto", "Udin", "Yuda")
+    ArMahasiswa.add("Yanti")
+    ArMahasiswa.remove("Udin")
+    ArMahasiswa.add("Udin") //udin jadi dipindahin di akhir krn udh di remove lalu di add lagi
+    println(ArMahasiswa)
 }
