e4e2fb5b5cdb12e2ba6bba05982c02a2db521d1e
kei <keishalai6@gmail.com>
2026-05-31 21:28:42 +0700

    week13: (task) implement toCsv for TradeRecord

--- src/oop_00000128606_keishaarialai/week13/CryptoTradeLogger.kt
+++ src/oop_00000128606_keishaarialai/week13/CryptoTradeLogger.kt
@@ -1,3 +1,7 @@
 package oop_00000128606_keishaarialai.week13
 
 data class TradeRecord(val id: Int, val symbol: String, val type: String, val margin: Double, val pnl: Double)
+
+// Serialization (Object -> CSV)
+fun TradeRecord.toCsv(): String = "$id,$symbol,$type,$margin,$pnl"
+
